a9f16ada4092c054fd6ad4f29dc96b267ee4baac
James McCreight <jamesmcc@ucar.edu>
2020-12-15 22:32:55 -0700

    figs 10 and 11

---
+++ figure_11.R
@@ -0,0 +1,45 @@
+
+fig_supp_table = 'figure'
+figure_number = '11'
+tag = ''
+location = 'pemigewasset_river'
+time_period = 'five_years'
+source('mk_file_name.R')
+
+data <- WtGetEventData(location, time_period)
+
+wt_event = WtEventTiming(
+  POSIXct=data$POSIXct,
+  obs=data$q_cms_obs,
+  mod=list(
+    'NWM v1.0'=data$`NWM v1.0`,
+    'NWM v1.1'=data$`NWM v1.1`,
+    'NWM v1.2'=data$`NWM v1.2`),
+  min_ts_length=456,
+  max.scale=256,
+  rm_chunks_warn=FALSE)
+
+## wt_event = WtEventTiming(
+##   POSIXct=data$POSIXct,
+##   obs=data$q_cms_obs,
+##   mod=list(
+##     'NWM v1.0'=data$`NWM v1.0`,
+##     'NWM v1.1'=data$`NWM v1.1`,
+##     'NWM v1.2'=data$`NWM v1.2`),
+##   min_ts_length=256,
+##   max.scale=256,
+##   rm_chunks_warn=FALSE
+## )
+
+we_stats <- we_hydro_stats(wt_event)
+
+figure <- event_cluster_timing_summary_by_period(
+    we_stats,
+    wt_event=wt_event,
+    n_period=3, 
+    distiller_pal='RdYlBu', 
+    timing_stat='cluster_max',
+    signif_threshold=.75
+)
+
+ggsave(file=figure_name, figure$ggplot)
