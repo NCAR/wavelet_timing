15235e6ffcda9848fd0ea7e3bfb68b9bfc76435a
James McCreight <jamesmcc@ucar.edu>
2020-12-15 21:32:49 -0700

    Figure6 cluster maxes

--- figure_06.R
+++ figure_06.R
@@ -1,3 +1,4 @@
+library(rwrfhydro); options(warn=1)
 
 fig_supp_table = 'figure'
 figure_number = '06'
@@ -18,4 +19,43 @@ wt_event = WtEventTiming(
 )
 
 figure <- event_cluster_timing_by_period(wt_event, n_period=5)
+
+## Add the cluster max to the figure
+## This could be added to the above function.
+we_stats = we_hydro_stats(wt_event)
+maxs = we_stats$`NWM v1.2`$xwt$event_timing$cluster_max
+maxs$period_str = as.character(maxs$period)
+maxs$period_factor = factor(maxs$period)
+maxs = maxs[period_str %in% figure$data$per_str]
+
+clust_numbers = unique(sort(maxs$period_clusters))
+clust_fill_colors = c(NA, rwrfhydro:::cluster_palette()(length(clust_numbers)))
+names(clust_fill_colors) = c('None', clust_numbers)
+
+ff = figure +
+    geom_vline(
+        data=maxs,
+        size=.5,
+        key_glyph = "path",
+        aes(xintercept=time_err,
+            linetype=xwt_signif, #'Cluster Maximum',
+            color=as.factor(period_clusters))) + 
+            # alpha=xwt_signif)) +
+    scale_color_manual(
+        values=clust_fill_colors,
+        name='Event Cluster Number',
+        na.translate=TRUE)  +
+    scale_linetype_manual(
+        ## values=c('Cluster Maximum'=1),
+        values=c('TRUE'=1, 'FALSE'=2),
+        ## name='')
+        labels=c('TRUE'='Hit', 'FALSE'='Miss'),
+        name='Cluster Max Event') +
+    guides(
+        fill = guide_legend(order = 1),
+        alpha = guide_legend(order = 2),        
+        linetype = guide_legend(order = 3),
+        color=FALSE)
+ff
+
 ggsave(file=figure_name, figure)
